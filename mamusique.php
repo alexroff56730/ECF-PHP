ae0f87b2916fd007eb07fd0e38e8205c11d76354
unknown <alexandreroffiaen@gmail.com>
2022-09-28 16:50:36 +0200

    init

--- mamusique.php
+++ mamusique.php
@@ -5,6 +5,47 @@
         header("Location: connexion.php");
     }
 
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+        echo "Le formulaire a été soumis <br />";
+        // Affichage des informations
+
+        echo "<br/>";
+
+        $nom_fichier = $_FILES["myFile"]["name"];
+        $type_fichier = $_FILES["myFile"]["type"];
+        $taille_fichier = $_FILES["myFile"]["size"];
+        $tmp_fichier = $_FILES["myFile"]["tmp_name"];
+        $error_fichier = $_FILES["myFile"]["error"];
+
+        // Vérification de l'extension du fichier
+        $extension_autorises = array("jpg" => "image/jpg", "jpeg" => "image/jpeg", "gif" => "image/gif", "png" => "image/png");
+
+        // La fonction "pathinfo" retourne des informations sur un chemin système
+        // On cherche a recupérer que l'extension de l'image en question
+        $extension_fichier = pathinfo($nom_fichier, PATHINFO_EXTENSION);
+
+        // On vérifie si l'extension (le key precisément) recupéré du fichier n'existe pas dans le tableau 
+        // des extensions autorisés
+        if (!array_key_exists($extension_fichier, $extension_autorises)) {
+            echo "Error : Merci de selectionner le bon format de fichier <br/>";
+        }
+
+        // On vérifie si le name attribut "photo" existe et qu'il n'y a pas d'erreur lors de l'uploading
+        if (isset($_FILES["myFile"]) && $_FILES["myFile"]["error"] == 0) {
+            // Je vérifie si l'extension du fichier fait partie des extensions autorisés
+            if (in_array($type_fichier, $extension_autorises)) {
+                // Je verifie aussi si le fichier existe dans le serveur, sinon je le crée un dossier 
+                // Pour enregistrer le fichier que je vais upload
+                if (file_exists("upload/" . $nom_fichier)) {
+                    echo "Le fichier existes.<br/>";
+                } else {
+                    move_uploaded_file($tmp_fichier, "upload/" . $nom_fichier);
+                    echo "Votre fichier a été uploadé correctement.<br/>";
+                }
+            }
+        }
+    }
+
     
     
 ?>
---
+++ sql/monsite.sql
@@ -0,0 +1,99 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Hôte : 127.0.0.1
+-- Généré le : mer. 28 sep. 2022 à 16:48
+-- Version du serveur : 10.4.24-MariaDB
+-- Version de PHP : 8.1.6
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données : `monsite`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `musique`
+--
+
+CREATE DATABASE `monsite`;
+
+USE `monsite`;
+
+CREATE TABLE `musique` (
+  `id` int(11) NOT NULL,
+  `Nom` char(25) NOT NULL,
+  `Description` varchar(255) NOT NULL,
+  `Musique` varchar(255) NOT NULL,
+  `PseudoUser` char(25) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE `user` (
+  `id` int(11) NOT NULL,
+  `Nom` char(25) NOT NULL,
+  `Prenom` char(25) NOT NULL,
+  `Mail` varchar(255) NOT NULL,
+  `Pseudo` char(25) NOT NULL,
+  `Mdp` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `Nom`, `Prenom`, `Mail`, `Pseudo`, `Mdp`) VALUES
+(1, 'ROFFIAEN', 'Alexandre', 'alexandreroffiaen@gmail.com', 'aroff56', '$2y$10$LH8iU72UJxS7QbwynYMSNuRemzIGGtv/JFIhlGfUjfaR3TGZTwSpC');
+
+--
+-- Index pour les tables déchargées
+--
+
+--
+-- Index pour la table `musique`
+--
+ALTER TABLE `musique`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables déchargées
+--
+
+--
+-- AUTO_INCREMENT pour la table `musique`
+--
+ALTER TABLE `musique`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT pour la table `user`
+--
+ALTER TABLE `user`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
